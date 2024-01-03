78b9f24c952d056eed7bcc390b4acc46e3bfa27a
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2024-01-03 14:42:19 +0300

    implement authorization endpoint logic

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 from markupsafe import escape
 from flask_wtf import CSRFProtect
 
 from forms import login, registration
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required
+from urllib.parse import urlencode
 
-
+import datetime
 import os
 import secrets
 import bcrypt
@@ -32,6 +33,7 @@ login_manager.login_view = "login"
 
 
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), nullable=False)
     last_name = db.Column(db.String(80), nullable=False)
@@ -40,6 +42,8 @@ class User(db.Model):
     is_authenticated = db.Column(db.Boolean, unique=False, default=False)
     is_active = db.Column(db.Boolean, unique=False, default=False)
     is_anonymous = db.Column(db.Boolean, unique=False, default=True)
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
     def hash_password(self):
         # Hash a password for the first time
@@ -70,18 +74,35 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.email_address
 
+
+class OauthClient(db.Model):
+
+    __tablename__ = 'oauth_clients'
+
+    id = db.Column(db.Integer, primary_key=True)
+    client_id = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(255), unique=True, nullable=False)
+    client_secret = db.Column(db.String(80), nullable=False)
+    endpoint = db.Column(db.String(255), unique=True, nullable=False)
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+
 # TODO Arrange the app
-
-
 class OauthCode(db.Model):
+    __tablename__ = 'oauth_codes'
     id = db.Column(db.Integer, primary_key=True)
     type = db.Column(db.String(80), nullable=False)
-    code = db.Column(db.String(80), nullable=False)
-    client_id = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
+    code = db.Column(db.String(255), nullable=False)
+    client_id = db.Column(db.Integer, db.ForeignKey('oauth_clients.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    expiry_time = db.Column(db.DateTime(timezone=True),
+                            default=datetime.datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
     def __repr__(self):
-        return '<OauthCode %r>' % self.first_name
+        return '<OauthCode %r>' % self.code
 
 
 # Form Protection - https://en.wikipedia.org/wiki/Cross-site_request_forgery
@@ -106,6 +127,13 @@ def unauthorized():
 
 with app.app_context():
     db.create_all()
+    default_client = OauthClient.query.filter_by(name='default').first()
+
+    if default_client is None:
+        client = OauthClient(
+            name="default", client_secret="client_secret", client_id="client_id", endpoint="https://oauth.pstmn.io/v1/callback")
+        db.session.add(client)
+        db.session.commit()
 
 
 @app.route("/")
@@ -122,6 +150,66 @@ def hello(name):
 @login_required
 def profile():
     return render_template('auth/profile.html')
+
+
+@app.route("/oauth/authorization", methods=["GET", "POST"])
+@login_required
+def authorization():
+    args = request.args
+    client_id = args.get('client_id')
+    redirect_uri = args.get('redirect_uri')
+    response_type = args.get('response_type')
+    code_challenge = args.get('code_challenge')
+    code_challenge_method = args.get('code_challenge_method')
+
+    if None not in (client_id, redirect_uri, response_type, code_challenge, code_challenge_method) and code_challenge_method == "S512":
+        # Try resolving the client from the database
+        client = OauthClient.query.filter_by(
+            client_id=client_id, endpoint=redirect_uri)
+
+        if client:
+            # When a client is found
+            if request.method == "POST" and request.form["accept"] == "1":
+
+                parameters = dict(code="message")
+                redirect_url = redirect_uri + \
+                    ("?" + urlencode(parameters) if parameters else "")
+                
+                return redirect(redirect_url)
+            
+            elif request.method == "POST" and request.form["accept"] == "0":
+
+                parameters = dict(error="access_denied", error_description="resource owner revoked request")
+                redirect_url = redirect_uri + \
+                    ("?" + urlencode(parameters) if parameters else "")
+                
+                return redirect(redirect_url)
+            
+            else:
+                return render_template('auth/consent.html', client=client)
+        else:
+            # When the provided params are not associated with a client
+            parameters = dict(error="unauthorized_client",
+                              error_description="invalid client id and redirect uri")
+            redirect_url = redirect_uri + \
+                ("?" + urlencode(parameters) if parameters else "")
+            return redirect(redirect_url)
+
+    elif None not in (redirect_uri):
+        # When the provided request params are invalid/dont contain all required params
+        parameters = dict(error="invalid_request")
+        redirect_url = redirect_uri + \
+            ("?" + urlencode(parameters) if parameters else "")
+
+        return redirect(redirect_url)
+
+    # Ultimatley redirect to the home page
+    return redirect(url_for('index'))
+
+
+@app.post("/oauth/token")
+def access_token_endpoint():
+    return "OK"
 
 
 @app.route("/register", methods=["GET", "POST"])
