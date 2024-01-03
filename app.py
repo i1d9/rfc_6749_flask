b686b324886ffae98ee6485f9aef7912f9b5ab15
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2024-01-03 18:32:28 +0300

    completed authorization_code grant type

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ from flask_login import LoginManager, login_user, logout_user, login_required, c
 from urllib.parse import urlencode
 
 from datetime import datetime, timedelta
-import datetime
 import os
 import secrets
 import bcrypt
+import base64
+import hashlib
+import jwt
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -43,8 +45,8 @@ class User(db.Model):
     is_authenticated = db.Column(db.Boolean, unique=False, default=False)
     is_active = db.Column(db.Boolean, unique=False, default=False)
     is_anonymous = db.Column(db.Boolean, unique=False, default=True)
-    updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def hash_password(self):
         # Hash a password for the first time
@@ -85,8 +87,8 @@ class OauthClient(db.Model):
     name = db.Column(db.String(255), unique=True, nullable=False)
     client_secret = db.Column(db.String(80), nullable=False)
     endpoint = db.Column(db.String(255), unique=True, nullable=False)
-    updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 
 # TODO Arrange the app
@@ -101,9 +103,9 @@ class OauthCode(db.Model):
     client_id = db.Column(db.Integer, db.ForeignKey('oauth_clients.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     expiry_time = db.Column(db.DateTime(timezone=True),
-                            default=datetime.datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+                            default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<OauthCode %r>' % self.code
@@ -166,10 +168,10 @@ def authorization():
     code_challenge = args.get('code_challenge')
     code_challenge_method = args.get('code_challenge_method')
 
-    if None not in (client_id, redirect_uri, response_type, code_challenge, code_challenge_method) and code_challenge_method == "S512":
+    if None not in [client_id, redirect_uri, response_type, code_challenge, code_challenge_method] and code_challenge_method == "S512":
         # Try resolving the client from the database
         client = OauthClient.query.filter_by(
-            client_id=client_id, endpoint=redirect_uri)
+            client_id=client_id, endpoint=redirect_uri).first()
 
         if client:
             # When a client is found, the request method is POST and the resource owner has accepted the request
@@ -179,12 +181,13 @@ def authorization():
                 minutes_later = timedelta(minutes=5)
                 code = secrets.token_urlsafe(24)
                 oauth_code = OauthCode(
+                    type=response_type,
                     client_code=code_challenge,
                     client_id=client.id, user_id=current_user.id, expiry_time=current_time + minutes_later, code=code)
 
                 db.session.add(oauth_code)
                 db.session.commit()
-                
+
                 parameters = dict(code=oauth_code.code)
                 redirect_url = redirect_uri + \
                     ("?" + urlencode(parameters) if parameters else "")
@@ -202,6 +205,7 @@ def authorization():
             # When a client is found, the request method is GET
             # This should be the default block that runs then the client is found
             else:
+
                 return render_template('auth/consent.html', client=client)
         else:
             # When the provided params(redirect_uri and client_id) are not associated with a client
@@ -211,7 +215,7 @@ def authorization():
                 ("?" + urlencode(parameters) if parameters else "")
             return redirect(redirect_url)
 
-    elif None not in (redirect_uri):
+    elif redirect_uri is not None:
         # When the provided request params are invalid/dont contain all required params
         parameters = dict(error="invalid_request")
         redirect_url = redirect_uri + \
@@ -224,28 +228,58 @@ def authorization():
 
 
 @app.post("/oauth/token")
+@csrf.exempt
 def access_token_endpoint():
-    args = request.args
+    args = request.json
+    
     grant_type = args.get('grant_type')
     code = args.get('code')
     redirect_uri = args.get('redirect_uri')
     client_id = args.get('client_id')
     code_verifier = args.get('code_verifier')
 
-    if None not in (client_id, redirect_uri, grant_type, code, code_verifier):
+
+    if None not in [client_id, redirect_uri, grant_type, code, code_verifier]:
         # Try resolving the client from the database
         client = OauthClient.query.filter_by(
-            client_id=client_id, endpoint=redirect_uri)
-        data = ""
+            client_id=client_id, endpoint=redirect_uri).first()
+        data = {}
+        current_time = datetime.now()
+        hour_later = timedelta(hours=1)
         if client:
             match grant_type:
                 case "authorization_code":
-                    if None in (code_verifier, code):
-                        data = {
-                            "access_token": "2YotnFZFEjr1zCsicMWpAA",
-                            "token_type": "Bearer",
-                            "expires_in": 3600,
-                        }
+                    if None not in  [code_verifier, code]:
+
+                        oauth_code = OauthCode.query.filter_by(
+                            code=code,
+                            client_id=client.id,  type="authorization_code").first()
+
+                        hash = hashlib.sha512(
+                            code_verifier.encode('UTF-8')).digest()
+
+                        computed_challenge = base64.urlsafe_b64encode(
+                            (hash)).decode("utf-8")
+
+                        if oauth_code and oauth_code.client_code == computed_challenge and oauth_code.expiry_time > current_time:
+
+                            encoded_jwt = jwt.encode(
+                                {'user_id': str(oauth_code.user_id)}, 'secret', algorithm='HS512')
+
+                            access_token = OauthCode(
+                                type="access_token",
+                                client_id=client.id, user_id=oauth_code.user_id, expiry_time=current_time + hour_later, code=encoded_jwt)
+
+                            data = {
+                                "access_token": access_token.code,
+                                "token_type": "Bearer",
+                                "expires_in": 3600,
+                            }
+                        else:
+                            
+                            data = {"error": "invalid_request",
+                                    "error_description": "invalid parameters provided",
+                                    }
                     else:
                         data = {"error": "invalid_request",
                                 "error_description": "invalid parameters provided",
@@ -276,7 +310,7 @@ def access_token_endpoint():
             "error_description": "invalid parameters provided",
             }
 
-    return jsonify(data, code=400)
+    return jsonify(data)
 
 
 @app.route("/register", methods=["GET", "POST"])
