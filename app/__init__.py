cc6e9ef0e661f57fd1f1d762f37346c43b5284f8
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2024-01-04 19:25:44 +0300

    separate csrf token, added flask_login current_user resolver

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,48 @@
 from flask import Flask
 from config import Config
+from app.extensions import db
+from app.models.user import User
+from flask_wtf import CSRFProtect
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+import secrets
 
+login_manager = LoginManager()
+csrf = CSRFProtect()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    foo = secrets.token_urlsafe(16)
+    app.secret_key = foo
+
+    csrf.init_app(app)  
+    db.init_app(app)
+
+
+    login_manager.init_app(app)
+    login_manager.session_protection = "strong"
+    login_manager.login_view = "auth.sign_in"
+
+
+
     # Register blueprints
     from app.authentication import auth_bp
-    app.register_blueprint(auth_bp)
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+
+    from app.oauth import oauth_bp
+    app.register_blueprint(oauth_bp, url_prefix="/oauth")
     
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        user = User.query.get(
+            user_id)
+
+        if user:
+            return user
+        else:
+            return None
 
     @app.route('/test')
     def test_page():
