a7c8c182c76036e0633e20133b04becd5aeac05c
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2024-01-04 19:37:38 +0300

    seperate login manager extension and csrf

--- app/extensions.py
+++ app/extensions.py
@@ -1,4 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+csrf = CSRFProtect()
